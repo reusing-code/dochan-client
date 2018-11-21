b185057378cc82c6bdaaf98f172386eb2d0033a1
Tobias Reusing <tobias@reusing.eu>
2018-11-21 21:04:04 +0100

    Setup project

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      greeting: 'Fick deine mudda'
+    };
+  }
+};
+</script>
+
+<style>
+</style>
