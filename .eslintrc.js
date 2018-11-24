ec4c270d36cea38f2dde3c74160a8e2510f55d44
Tobias Reusing <tobias@reusing.eu>
2018-11-24 14:33:45 +0100

    Ran lint on all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", 'plugin:prettier/recommended',"@vue/prettier"],
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'vue/html-indent': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
 };
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,27 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <b-link to="/" class="navbar-brand">Dochan</b-link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <b-link to="/" class="navbar-brand">Dochan</b-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
         <li class="nav-item active-class">
-          <b-link to="/" class="nav-link" exact>Home</b-link> 
-      </li>
-      <li class="nav-item active-class">
-        <b-link to="/about" class="nav-link" exact>About</b-link>
-      </li>
+          <b-link :to="{ name: 'home' }" class="nav-link" exact>Home</b-link> 
+        </li>
+        <li class="nav-item active-class">
+          <b-link :to="{ name: 'about' }" class="nav-link" exact>About</b-link>
+        </li>
       
-    </ul>
-  </div>
-</nav>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style scoped>
-
 </style>
