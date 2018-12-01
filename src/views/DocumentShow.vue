c00f0f5c95a5f0dd06a2e8d84a4f55f7454cfe81
Tobias Reusing <tobias@reusing.eu>
2018-12-01 13:01:43 +0100

    Add pdf viewing

--- src/views/DocumentShow.vue
+++ src/views/DocumentShow.vue
@@ -1,36 +1,40 @@
 <template>
   <div>
     <template v-if="error">
-      <div class="alert alert-danger" role="alert">
-        Error: document not found
-      </div>
-      
+      <div class="alert alert-danger" role="alert">Error: document not found</div>
     </template>
-    <template v-else="">
+    <template v-else>
       <template v-if="document">
         <p>Document: {{document.filename}}</p>
+        <pdf :src="pdfSrc"/>
       </template>
       <template v-else>
-        <p>... Loading ... </p>
+        <p>... Loading ...</p>
       </template>
     </template>
   </div>
 </template>
 
 <script>
+import pdf from 'vue-pdf';
 import DocumentService from '@/services/DocumentService.js';
 export default {
   props: ['id'],
+  components: {
+    pdf
+  },
   data() {
     return {
       document: null,
-      error: null
+      error: null,
+      pdfSrc: null
     };
   },
   created() {
     DocumentService.getDocument(this.id)
       .then(response => {
         this.document = response.data;
+        this.pdfSrc = `http://localhost:8092/api/documents/${this.id}/download`;
       })
       .catch(error => {
         this.error = error;
