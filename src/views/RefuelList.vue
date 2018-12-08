7746eb68b0e9a80610e1dd0d93b98a6b86c5fa13
Tobias Reusing <tobias@reusing.eu>
2018-12-08 22:13:45 +0100

    Add custom fields for table

--- src/views/RefuelList.vue
+++ src/views/RefuelList.vue
@@ -3,8 +3,20 @@
     <template v-if="error">
       <div class="alert alert-danger" role="alert">Error: no fuel records found</div>
     </template>
-    <template v-else-if="fuelRecords && fuelRecords.length > 0">
-      <b-table striped hover :items="fuelRecords"></b-table>
+    <template v-else-if="fuelRecords">
+      <b-table striped hover :fields="fields" :items="fuelRecords">
+        <template
+          slot="costCent"
+          slot-scope="data"
+        >{{Math.floor(data.value / 100)}}.{{(data.value % 100).toString().padStart(2, '0')}} €</template>
+        <template slot="location" slot-scope="data">
+          <a
+            v-if="data.value != null"
+            :href="`https://www.google.com/maps/search/?api=1&query=${data.value}`"
+          >Location</a>
+          <div v-else>-</div>
+        </template>
+      </b-table>
     </template>
     <template v-else>No fuel records available</template>
   </div>
@@ -17,6 +29,31 @@ export default {
   components: {},
   data() {
     return {
+      fields: [
+        {
+          key: 'date',
+          formatter: value => {
+            var d = new Date(value);
+            return d.toLocaleString('de-DE');
+          }
+        },
+        { key: 'costCent' },
+        {
+          key: 'location',
+          formatter: (value, key, item) => {
+            if (item === undefined || item === null) {
+              return null;
+            }
+            var lat = item.lat;
+            var lon = item.lon;
+            if (lat != '0' && lon != '0') {
+              return `${lat},${lon}`;
+            } else {
+              return null;
+            }
+          }
+        }
+      ],
       fuelRecords: null,
       error: null
     };
